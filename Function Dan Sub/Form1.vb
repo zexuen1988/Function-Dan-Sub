2cbf364f0b8643de9d8e1ebcd11d4dcdefde12eb
Lim Ze Xuen <56021775+zexuen1988@users.noreply.github.com>
2021-04-20 13:07:15 +0800

    Contoh Function and Sub

--- Function Dan Sub/Form1.vb
+++ Function Dan Sub/Form1.vb
@@ -3,12 +3,12 @@
 
     End Sub
 
-
     Private Sub sub1()
         lblLabel1.Text = "Hi"
     End Sub
 
-    Private Sub sub2(strText1 As String)
+    Private Sub sub2()
+        Dim strText1 As String
         strText1 = txtBox1.Text
         lblLabel2.Text = strText1
     End Sub
@@ -18,11 +18,26 @@
         Return lblLabel3
     End Function
 
-    Private Function fun2(strText2 As String)
+    Private Function fun2()
+        Dim strText2 As String
         strText2 = txtBox2.Text
         lblLabel4.Text = strText2
         Return lblLabel4
     End Function
 
+    Private Sub BtnButton1_Click(sender As Object, e As EventArgs) Handles BtnButton1.Click
+        sub1()
+    End Sub
 
+    Private Sub BtnButton2_Click(sender As Object, e As EventArgs) Handles BtnButton2.Click
+        sub2()
+    End Sub
+
+    Private Sub BtnButton3_Click(sender As Object, e As EventArgs) Handles BtnButton3.Click
+        fun1()
+    End Sub
+
+    Private Sub BtnButton4_Click(sender As Object, e As EventArgs) Handles BtnButton4.Click
+        fun2()
+    End Sub
 End Class
